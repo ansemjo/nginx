4872b818ea713ebad5d37db8a2e5ab13fd5c34c3
Anton Semjonov <anton@semjonov.de>
2016-02-03 19:16:27 +0100

    new location layout; also using symlinks now

--- locations/.compile.sh
+++
@@ -1,9 +0,0 @@
-LOCATION="/etc/nginx/locations"
-FILE="$LOCATION/.includeall"
-
-printf "### $(date)\n\n" > $FILE 
-
-for NAME in $(ls $LOCATION);
-do
-	printf "### $NAME\ninclude $LOCATION/$NAME;\n\n" >> $FILE
-done;
